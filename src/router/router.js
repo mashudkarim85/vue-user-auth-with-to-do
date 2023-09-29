a857f0ddf1cebc429c70f56f6a2fa733c5e99d08
mashud.karim <mashud.karim85@gmail.com>
2023-09-29 21:12:47 +0600

    update auth

--- src/router/router.js
+++ src/router/router.js
@@ -2,15 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Register from '../components/Register.vue'
 import Login from '../components/Login.vue'
 import Dashboard from '../components/Dashboard.vue'
+import { useAuthStore } from '../store/AuthStore.js'
 
 const routes = [
     {
         path: '/',
-        redirectTo: 'login'
+        redirect: 'login'
     },
     {
         path: '/login',
-        name: 'Login',
+        name: 'Login',  
         component: Login
     },
     {
@@ -21,13 +22,25 @@ const routes = [
     {
         path: '/dashboard',
         name: 'Dashboard',
-        component: Dashboard
+        component: Dashboard,
+         meta:{
+            requiresAuth: true
+        }
     },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    const auth = useAuthStore()
+    if(to.meta.requiresAuth && !auth.isAuthenticated){
+        next('/login')
+    }else{
+        next()
+    }
 })
 
 export default router
